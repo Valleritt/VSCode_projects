42203048d1a924594886807e305777f91e6ed33b
Valleritt <jurie114@hotmail.com>
2022-02-10 14:48:06 +0000

    New Project Save.

---
+++ Adopt-me/src/App.js
@@ -0,0 +1,22 @@
+const App = () => {
+    return React.createElement("div", {}, [
+        React.createElement("h2", {}, "Luna"),
+        React.createElement("h3", {}, "Dog"),
+        React.createElement("h2", {}, "Havanese"),
+    ])
+}
+
+const App = () => {
+    return React.createElement(
+        "div",
+        {},
+        [
+            React.createElement("h1", {}, "Adopt Me!"),
+            React.createElement(Pet),
+            React.createElement(Pet),
+            React.createElement(Pet),
+        ]
+      );
+    };
+
+    ReactDOM.render(React.createElement(App), document.getElementById("root"));
