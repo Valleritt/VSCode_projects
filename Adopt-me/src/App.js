378892e87f6922a3445541f8dce5018590b26e04
Valleritt <jurie114@hotmail.com>
2022-02-10 15:56:54 +0000

    Mod

--- Adopt-me/src/App.js
+++ Adopt-me/src/App.js
@@ -1,8 +1,8 @@
-const App = () => {
+const Pet = (props) => {
     return React.createElement("div", {}, [
-        React.createElement("h2", {}, "Luna"),
-        React.createElement("h3", {}, "Dog"),
-        React.createElement("h2", {}, "Havanese"),
+        React.createElement("h2", {}, props.name),
+        React.createElement("h3", {}, props.animal),
+        React.createElement("h2", {}, props.breed),
     ])
 }
 
@@ -11,10 +11,10 @@ const App = () => {
         "div",
         {},
         [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet),
-            React.createElement(Pet),
-            React.createElement(Pet),
+            React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
+            React.createElement(Pet, { name: "Fluffy", animal: "Dog", breed: "Labrador Retriever"} ),
+            React.createElement(Pet, { name: "Pepper", animal: "Bird", breed: "Cockatiel"} ),
+            React.createElement(Pet, { name: "Jackie", animal: "Cat", breed: "Persian"} ),
         ]
       );
     };
